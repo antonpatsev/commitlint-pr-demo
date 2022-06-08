14765b9133546560930d4180bec2a7b44437a2ee
Anton Patsev <a.patsev@sxope.com>
2022-06-08 16:11:56 +0600

    'subject-empty': [0, 'never'],

--- commitlint.config.js
+++ commitlint.config.js
@@ -6,7 +6,29 @@ module.exports = {
     parserPreset: {
       parserOpts: {
         headerPattern: /\(DEVOPS[^)]*\)$/,
-        headerCorrespondence: ['type', 'scope', 'subject']
+        headerCorrespondence: ['scope']
       }
-    }
+    },
+        rules: {
+          'type-case': [0, 'never'],
+          'subject-case': [0, 'never'],
+          'subject-empty': [0, 'never'],
+          'type-enum': [
+            2,
+            'always',
+            [
+              'build',
+              'ci',
+              'chore',
+              'docs',
+              'feat',
+              'fix',
+              'perf',
+              'refactor',
+              'revert',
+              'style',
+              'test',
+            ]
+          ]
+        }
   };
